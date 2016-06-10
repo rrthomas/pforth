8def0b3a2f829167f60bb4103e9dd4d1e561f630
Reuben Thomas <rrt@sc3d.org>
2016-06-10 13:37:27 +0100

    Beetle meta.fs: pull non-redefined words out of list of redefiners

--- src/beetle/meta.fs
+++ src/beetle/meta.fs
@@ -39,6 +39,10 @@ ALSO ASSEMBLER
 HEX
 : EXECUTE   STATE @ IF  46 C,  ALIGN  ELSE  EXECUTE  THEN ; IMMEDIATE
 : @EXECUTE   STATE @ IF  47 C,  ALIGN  ELSE  @EXECUTE  THEN ; IMMEDIATE
+: OFFSET   ( from to -- offset )   OVER M0 <  OVER M0 <  OR ABORT" OFFSET out of image!"
+   >-<  CELL/ 1-  00FFFFFF AND ;
+: (ERROR")   -512 THROW ;
+: ERROR"   POSTPONE S"  POSTPONE (ERROR") ; IMMEDIATE COMPILING
 
 R: <M0   M0 - 10 + ;
 R: >M0   M0 + 10 - ;
@@ -46,8 +50,6 @@ R: AHEAD   HERE  42 C,  0 FIT,  0 , ;
 R: IF   HERE  44 C,  0 FIT,  0 , ;
 R: LITERAL   B(LITERAL) ; \ FIXME: use same definition as in machdeps.fs
 R: NOPALIGN   0 FIT, ; \ Use for 0 FIT, in the metacompiler
-: OFFSET   ( from to -- offset )   OVER M0 <  OVER M0 <  OR ABORT" OFFSET out of image!"
-   >-<  CELL/ 1-  00FFFFFF AND ;
 R: !BRANCH   ( at from to opcode -- )   -ROT  OFFSET  8 LSHIFT  OR  SWAP ! ;
 R: BRANCH   ( at from to -- )   43 !BRANCH ;
 R: CALL   ( at from to -- )   49 !BRANCH ;
@@ -74,8 +76,6 @@ R: >DOES   ( xt -- adr ) CELL+ ;
 R: (DOES>)   LAST >DOES  DUP  R> @  BRANCH ;
 R: DOES>   POSTPONE (DOES>) ALIGN  HERE CELL+  LAST  2DUP - CELL/  SWAP >INFO
    DUP @ ROT OR  SWAP !  <M0 , ; IMMEDIATE
-: (ERROR")   -512 THROW ;
-: ERROR"   POSTPONE S"  POSTPONE (ERROR") ; IMMEDIATE COMPILING
 R: (POSTPONE)   R>  03FFFFFF AND  DUP 4 + >R  @ >NAME FIND  0= IF  ERROR" ?"
    THEN  COMPILE, ;
 R: POSTPONE   BL WORD FIND  ?DUP 0= IF  ERROR" ?"  THEN  0> IF  COMPILE,
