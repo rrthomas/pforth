3b314613deec14f1a01fc636664877e4b04afd57
Reuben Thomas <rrt@sc3d.org>
2016-06-11 22:46:43 +0100

    beetle/assembler.fs: add CODE,
    Neither CODE! nor CODE, is used nor required on Beetle, but defining
    them allows them to be used by assemblers for other platforms.

--- src/beetle/machdeps.fs
+++ src/beetle/machdeps.fs
@@ -15,8 +15,12 @@
 
 \ Writing code to memory
 
+\ These words are not needed or used for Beetle (until such time as CPUs
+\ start requiring special privileges for VM instructions?!), but are
+\ provided so they can be used when cross-compiling with assemblers for
+\ systems that need them when running natively.
 : CODE!   ( x adr -- )   ! ;
-\ FIXME: add CODE, and use CODE! and CODE, as for ARM
+: CODE,   ( x -- )   , ;
 
 
 \ Branches
