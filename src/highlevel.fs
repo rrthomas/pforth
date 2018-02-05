8ce75d1088004174171047654d896d3d2319e99c
Reuben Thomas <rrt@sc3d.org>
2018-02-05 13:43:42 +0000

    Move SAVE-FILE to riscos/save.fs
    Yes, it’s RISC OS *SAVE, but it’s not sufficiently generally useful: it’s
    easy enough to open a file and write some bytes to it, and often one wants
    to write more than one block (as we do when meta-compiling pForth for
    Beetle).

--- src/highlevel.fs
+++ src/highlevel.fs
@@ -226,13 +226,6 @@ INCLUDE" control2.fs"
 
 INCLUDE" fileio.fs"
 
-\ Save u1 bytes at a-addr in the file given by c-addr u2
-: SAVE-FILE   ( a-addr u1 c-addr u2 -- )
-   W/O CREATE-FILE DROP          \ open file
-   >R                            \ save file-id
-   R@ WRITE-FILE DROP            \ write data
-   R> CLOSE-FILE DROP ;          \ close file
-
 : READ-LINE   ( c-addr u1 fid -- u2 flag ior )
    >R  OVER SWAP                     \ save fid and copy c-addr
    R@ READ-FILE                      \ fill buffer
--- src/riscos/save.fs
+++ src/riscos/save.fs
@@ -1,4 +1,12 @@
 \ Save an object file
+
+\ Save u1 bytes at a-addr in the file given by c-addr u2
+: SAVE-FILE   ( a-addr u1 c-addr u2 -- )
+   W/O CREATE-FILE DROP          \ open file
+   >R                            \ save file-id
+   R@ WRITE-FILE DROP            \ write data
+   R> CLOSE-FILE DROP ;          \ close file
+
 '? TYPE-FILE VALUE 'TYPE-FILE
 HEX
 : SAVE   ( a-addr u1 c-addr u2 -- )
