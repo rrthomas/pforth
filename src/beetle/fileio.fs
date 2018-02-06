659545abeed9a5deec5ddc7a83f63940973489d2
Reuben Thomas <rrt@sc3d.org>
2018-02-06 12:39:43 +0000

    beetle/fileio.fs: no longer convert filenames to NUL-terminated strings
    C Beetle has been updated to accept counted strings instead.

--- src/beetle/fileio.fs
+++ src/beetle/fileio.fs
@@ -1,5 +1,5 @@
 \ Mass storage input/output words
-\ Reuben Thomas   2/7/96-3/6/16
+\ Reuben Thomas   started 2/7/96
 
 : BIN ; IMMEDIATE
 CREATE R/O  CHAR r C, CHAR b C, 0 C,
@@ -7,12 +7,12 @@ CREATE W/O  CHAR w C, CHAR b C, 0 C,
 CREATE R/W  CHAR r C, CHAR + C, CHAR b C, 0 C,
 CREATE CREATE-FAM  CHAR w C, CHAR + C, CHAR b C, 0 C,
 
-: OPEN-FILE   ( adr u fam -- fid ior )   -ROT C0END SWAP  4 LIB ;
+: OPEN-FILE   ( adr u fam -- fid ior )   4 LIB ;
 : CLOSE-FILE   ( fid -- ior )   5 LIB ;
 
 : CREATE-FILE   ( adr u fam -- fid ior )   DROP  CREATE-FAM OPEN-FILE ;
-: RENAME-FILE   ( adr1 u1 adr2 u2 -- ior )   C0END -ROT C0END  SWAP 11 LIB ;
-: DELETE-FILE   ( adr u -- ior )   C0END 12 LIB ;
+: RENAME-FILE   ( adr1 u1 adr2 u2 -- ior )   11 LIB ;
+: DELETE-FILE   ( adr u -- ior )   12 LIB ;
 
 : READ-FILE   ( adr u1 fid -- u2 ior )   6 LIB ;
 : WRITE-FILE   ( adr u fid -- ior )   7 LIB ;
