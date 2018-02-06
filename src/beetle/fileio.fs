33a078426383a8f9d25a618fb80ee8508487d4f9
Reuben Thomas <rrt@sc3d.org>
2018-02-06 13:14:56 +0000

    beetle/fileio.fs: use a bitfield for file access methods
    Also make BIN do something sensible.
    
    This tracks a change to Beetle.

--- src/beetle/fileio.fs
+++ src/beetle/fileio.fs
@@ -1,16 +1,17 @@
 \ Mass storage input/output words
 \ Reuben Thomas   started 2/7/96
 
-: BIN ; IMMEDIATE
-CREATE R/O  CHAR r C, CHAR b C, 0 C,
-CREATE W/O  CHAR w C, CHAR b C, 0 C,
-CREATE R/W  CHAR r C, CHAR + C, CHAR b C, 0 C,
-CREATE CREATE-FAM  CHAR w C, CHAR + C, CHAR b C, 0 C,
+4 CONSTANT BIN-MODE
+: BIN  BIN-MODE OR ;
+1 CONSTANT R/O
+2 CONSTANT W/O
+3 CONSTANT R/W
+0 CONSTANT CREATE-FAM
 
 : OPEN-FILE   ( adr u fam -- fid ior )   4 LIB ;
 : CLOSE-FILE   ( fid -- ior )   5 LIB ;
 
-: CREATE-FILE   ( adr u fam -- fid ior )   DROP  CREATE-FAM OPEN-FILE ;
+: CREATE-FILE   ( adr u fam -- fid ior )   BIN-MODE AND  CREATE-FAM OR  OPEN-FILE ;
 : RENAME-FILE   ( adr1 u1 adr2 u2 -- ior )   11 LIB ;
 : DELETE-FILE   ( adr u -- ior )   12 LIB ;
 
