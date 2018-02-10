503ceef6aabaa814c10fa757e56a6743d875ed7e
Reuben Thomas <rrt@sc3d.org>
2018-02-10 20:54:42 +0000

    Since extended image only made for Beetle, don’t need filename shenanigans

--- src/make.fs
+++ src/make.fs
@@ -1,7 +1,6 @@
 #!/usr/bin/env pforth
 \ Create pForth distribution image
-\ Reuben Thomas   started 15/4/96
 
 INCLUDE" assembler.fs"
 
-S" pforth-img" SAVE-IMAGE   \ write system image
+S" pforth.img" SAVE-IMAGE   \ write system image
