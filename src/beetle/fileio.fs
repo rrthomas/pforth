8159bb25acfc5b9ccf73203c7d320863dfcc5a93
Reuben Thomas <rrt@sc3d.org>
2018-02-13 17:56:27 +0000

    beetle/fileio.fs: change I/O functions; match Beetle commit 1d39861

--- src/beetle/fileio.fs
+++ src/beetle/fileio.fs
@@ -1,17 +1,19 @@
 \ Mass storage input/output words
 \ Reuben Thomas   started 2/7/96
 
-4 CONSTANT BIN-MODE
+0 CONSTANT R/O
+1 CONSTANT W/O
+2 CONSTANT R/W
+
+4 CONSTANT CREATE-FAM
+
+8 CONSTANT BIN-MODE
 : BIN  BIN-MODE OR ;
-1 CONSTANT R/O
-2 CONSTANT W/O
-3 CONSTANT R/W
-0 CONSTANT CREATE-FAM
 
 : OPEN-FILE   ( adr u fam -- fid ior )   4 LIB ;
 : CLOSE-FILE   ( fid -- ior )   5 LIB ;
 
-: CREATE-FILE   ( adr u fam -- fid ior )   BIN-MODE AND  CREATE-FAM OR  OPEN-FILE ;
+: CREATE-FILE   ( adr u fam -- fid ior )   CREATE-FAM OR  OPEN-FILE ;
 : RENAME-FILE   ( adr1 u1 adr2 u2 -- ior )   11 LIB ;
 : DELETE-FILE   ( adr u -- ior )   12 LIB ;
 
