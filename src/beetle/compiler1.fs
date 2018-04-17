7c9ba7bb1675c6a6dba0f18043ba4200a6d86c03
Reuben Thomas <rrt@sc3d.org>
2018-04-17 12:17:14 +0100

    Match Beetle commit 90463ec that reorders the opcodes

--- src/beetle/compiler1.fs
+++ src/beetle/compiler1.fs
@@ -3,26 +3,26 @@
 : R>ADDRESS ; IMMEDIATE COMPILING
 : (POSTPONE)   R> R>ADDRESS  DUP CELL+ >R  @ CURRENT-COMPILE, ;
 
-: DO,   $4B C, ;
-: LOOP,   $4C ADR, ;
-: +LOOP,   $4E ADR, ;
+: DO,   $55 C, ;
+: LOOP,   $56 ADR, ;
+: +LOOP,   $58 ADR, ;
 : UNLOOP, ;
 
 ALSO ASSEMBLER
-: EXECUTE   STATE @ IF  $46 C,  NOPALIGN  ELSE [ $46 C,  NOPALIGN ]
+: EXECUTE   STATE @ IF  $50 C,  NOPALIGN  ELSE [ $50 C,  NOPALIGN ]
    THEN ; IMMEDIATE
-: @EXECUTE   STATE @ IF  $47 C,  NOPALIGN  ELSE [ $47 C,  NOPALIGN ]
+: @EXECUTE   STATE @ IF  $51 C,  NOPALIGN  ELSE [ $51 C,  NOPALIGN ]
    THEN ; IMMEDIATE
 PREVIOUS
 
 
 \ Data structures
 
-: LITERAL   DUP HERE 1+ FITS IF  $53 C, FIT,  ELSE $52 C,  NOPALIGN  ,  THEN ;
+: LITERAL   DUP HERE 1+ FITS IF  $5D C, FIT,  ELSE $5C C,  NOPALIGN  ,  THEN ;
 IMMEDIATE COMPILING
 
 \ Leave UNLINK, in next cell where it can be patched by DOES>
-: CREATE,   LINK,  $56 C,  $23 C,  NOPALIGN  UNLINK,  NOPALIGN ;
+: CREATE,   LINK,  $42 C,  $23 C,  NOPALIGN  UNLINK,  NOPALIGN ;
 : >BODY   2 CELLS + ;
 ( >DOES given an execution token returns the address of the branch to the
 DOES> code. There is always at least an aligned cell after this address free
