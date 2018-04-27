cbf2bc731a781b8e2bbea2cad5dd557cbedeb9df
Reuben Thomas <rrt@sc3d.org>
2018-04-27 22:28:42 +0100

    Add PROVIDE: to define a word if not in CURRENT
    Usage:
    
    PROVIDE: FOO … ; [THEN]
    
    Add high level definitions of MAX and MIN using PROVIDE:.

--- src/compiler4.fs
+++ src/compiler4.fs
@@ -1,6 +1,17 @@
 \ Compiler #4
 
 : :   BL WORD HEADER  TRUE SMUDGE  LINK,  ] ;
+: CURRENT?   ( wid xt n -- f )   2DROP  GET-CURRENT = ;
+: PROVIDE:
+   BL WORD
+   DUP ['] CURRENT? SELECT NIP IF
+      DROP
+      POSTPONE [ELSE]
+   ELSE
+      HEADER
+      TRUE SMUDGE
+      LINK, ]
+   THEN ;
 : ;   UNLINK,  POSTPONE [  FALSE SMUDGE ; IMMEDIATE COMPILING
 : :NONAME   ALIGN HERE LINK,  ] ;
 \ FIXME: This is the only use for a separate compilation method
