986414747ca8d5a3ee011b3af20c0f601a9e152a
Reuben Thomas <rrt@sc3d.org>
2018-07-23 01:06:27 +0100

    src/{beetle,smite}/compiler1.fs: fix white space

--- src/smite/compiler1.fs
+++ src/smite/compiler1.fs
@@ -25,5 +25,5 @@ IMMEDIATE COMPILING
 ( >DOES given an execution token returns the address of the branch to the
 DOES> code. There is always at least an aligned cell after this address free
 for messing around, although adr itself may not be aligned. )
-: >DOES   ( xt -- adr ) CELL+ ;
+: >DOES   ( xt -- adr )   CELL+ ;
 : (DOES>)   LAST >DOES  DUP  R> R>ADDRESS @  BRANCH ;
