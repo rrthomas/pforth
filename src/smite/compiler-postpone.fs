25bb7b0e7ec41864ce717cd54bacf153cf961de7
Reuben Thomas <rrt@sc3d.org>
2018-07-25 00:46:30 +0100

    SMite: remove (DO) instruction

--- src/smite/compiler-postpone.fs
+++ src/smite/compiler-postpone.fs
@@ -1,1 +1,12 @@
+\ Compiler words that need special treatment during meta-compilation owing
+\ to their use of POSTPONE
+
+\ Compiler
+
+\ FIXME: Common up with riscos, as defaults for platforms without special versions?
+: DO,   POSTPONE 2>R ; COMPILING
+
+
+\ Data structures
+
 : CREATE,   LINK,  NOPALIGN  POSTPONE (CREATE)  UNLINK,  NOPALIGN ;
