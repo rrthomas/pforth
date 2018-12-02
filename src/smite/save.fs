3afc7439d567b8b806e99260666c1585850f86dd
Reuben Thomas <rrt@sc3d.org>
2018-12-02 20:22:04 +0000

    src/smite/save.fs: fix a whitespace glitch

--- src/smite/save.fs
+++ src/smite/save.fs
@@ -2,7 +2,7 @@
 : SAVE-OBJECT   ( a-addr u1 c-addr u2 -- )
    W/O CREATE-FILE DROP          \ open file
    >R                            \ save file-id
-   S" SMITE" R@ WRITE-FILE DROP \ write header
+   S" SMITE" R@ WRITE-FILE DROP  \ write header
    0 SCRATCH TUCK C!  1  2DUP  R@ WRITE-FILE DROP
    2DUP  R@ WRITE-FILE DROP  R@ WRITE-FILE DROP
    DUP SCRATCH TUCK !  CELL R@ WRITE-FILE DROP
