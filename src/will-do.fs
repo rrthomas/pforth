1226065677d844e9de0a5c32188b310eb2d7e4a2
Reuben Thomas <rrt@sc3d.org>
2019-02-13 00:15:18 +0000

    src/will-do.fs: fix misleading line break in comment

--- src/will-do.fs
+++ src/will-do.fs
@@ -2,8 +2,8 @@
 \ code. Use as RESOLVER X WILL-DO Y, where Y must be a defining word. This
 \ can be used both to revector existing defining words, and to resolve the
 \ run-time code of new ones. Use RESOLVES or RESOLVE: to resolve WILL-DO,
-\ e.g.: ' NEW-DEFINER >DOES> RESOLVES DEFINER-RESOLVER WILL-DO must be used
-\ as part of a REDEFINER.
+\ e.g.: ' NEW-DEFINER >DOES> RESOLVES DEFINER-RESOLVER
+\ WILL-DO must be used as part of a REDEFINER.
 : WILL-DO   ( -- old new )   ' >DOES> #BRANCH-CELLS 1+ CELLS -  :NONAME POSTPONE LAST
    POSTPONE >DOES POSTPONE HERE POSTPONE SWAP POSTPONE DP POSTPONE !
    LAST CURRENT-COMPILE,  POSTPONE DP POSTPONE ! POSTPONE ; ;
