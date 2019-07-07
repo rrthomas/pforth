49714763b4a92d3b4a467b243cade84a7e5a0bd6
Reuben Thomas <rrt@sc3d.org>
2019-07-08 00:23:24 +0100

    RISC OS: fix a FIXME: move PRIMITIVE defining words to util.fs

---
+++ src/riscos/util.fs
@@ -0,0 +1,34 @@
+\ Define a primitive which takes x arguments and returns y results.
+\
+\ Usage:
+\
+\ x y PRIMITIVE FOO
+\ ( arg2 arg1 -> TOP=arg1, R0=arg2 )
+\ ( assembly code )
+\ ( TOP=result1, R0=result2 -> result2 result1 )
+\ END-PRIMITIVE \ or END-PRIMITIVE-CODE, if explicit flow control is needed
+
+: PRIMITIVE   ( args results -- results args )
+   CODE                       \ make a code word
+   SWAP                       \ ( results args )
+   DUP 3 < INVERT ABORT" PRIMITIVE needs < 3 arguments"
+   OVER  3 < INVERT ABORT" PRIMITIVE needs < 3 results"
+   2DUP  0=  SWAP 0<>  AND IF \ push cached stack item if args=0 & results<>0
+      TOP SP PUSH,
+   THEN
+   DUP 1 > IF                 \ pop second argument if args>1
+      R0 SP POP,
+   THEN ;
+
+: END-PRIMITIVE-CODE   ( results args -- )
+   OVER  2 = IF               \ push R0 if results=2
+      R0 SP PUSH,
+   THEN
+   2DUP 0>  SWAP 0=  AND IF   \ pop TOP if args>0 & results=0
+      TOP SP POP,
+   THEN
+   2DROP ;
+
+: END-PRIMITIVE
+   END-PRIMITIVE-CODE
+   END-SUB ;
