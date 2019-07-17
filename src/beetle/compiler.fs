8d9e8d8fea9ff5db26b564f1f2acd54f987ccfed
Reuben Thomas <rrt@sc3d.org>
2019-07-17 18:01:22 +0100

    Beetle: use relative (I) branches for all immediate branches

--- src/beetle/compiler.fs
+++ src/beetle/compiler.fs
@@ -8,6 +8,7 @@
 \ FIXME: once assembler "built-in", remove the following
 : FITS   ( x addr -- flag )   DUP ALIGNED >-<  DUP IF  8 * 1-  1 SWAP LSHIFT
    SWAP DUP 0< IF  INVERT  THEN  U>  ELSE NIP  THEN ;
+\ FIXME: FIT, should abort on failure
 : FIT,   ( x -- )   HERE DUP ALIGNED >-<  0 ?DO  DUP C,  8 RSHIFT  LOOP
    DROP ;
 : NOPALIGN   0 FIT, ;
@@ -15,25 +16,24 @@
 
 \ Core compiler
 
-: AHEAD   HERE  $4C C,  NOPALIGN  0 , ; IMMEDIATE COMPILING
-: IF   HERE  $4E C,  NOPALIGN  0 , ; IMMEDIATE COMPILING
-
 : OFFSET   ( from to -- offset )   >-<  CELL/ 1-  $00FFFFFF AND ;
-: @BRANCH   ( from -- to )   1+ ALIGNED @ >'FORTH ;
-: !BRANCH   ( at from to opcode -- )   HERE >R  >R  ROT DP !  R> C,  OFFSET
-   FIT,  R> DP ! ;
+: @BRANCH   ( from -- to )   DUP 1+ CELL- ALIGNED @  8 ARSHIFT  1+ CELLS + ;
+: !BRANCH   ( at from to -- )   OFFSET  HERE  ROT DP !  1 ALLOT
+   SWAP FIT,  DP ! ;
 
 \ FIXME: allow arbitrary branches; at the moment we're effectively
 \ restricted to 64Mb.
-: BRANCH   ( at from to -- )   $4D !BRANCH ;
+: BRANCH   ( at from to -- )   $4D 3 PICK C!  !BRANCH ;
 
-: JOIN   ( from to -- )   <'FORTH  SWAP  1+ ALIGNED  ! ;
-
-: ADR,   ( to opcode -- )   OVER HERE 1+ ALIGNED - CELL/  DUP HERE 1+ FITS
-   IF  SWAP 1+ C, FIT,  DROP  ELSE DROP C,  NOPALIGN  <'FORTH ,  THEN ;
+: ADR,   ( to opcode -- )   HERE ROT OFFSET  TUCK HERE 1+ FITS
+   INVERT IF  NOPALIGN  THEN  1+ C, FIT, ;
 : CALL,   ( to -- )   $52 ADR, ;
 : COMPILE,   DUP >INFO 2 + C@  ?DUP IF  0 DO  DUP C@ C,  1+  LOOP  DROP
    ELSE CALL,  THEN ;
+
+: AHEAD   NOPALIGN  HERE DUP $4C ADR, ; IMMEDIATE COMPILING
+: IF   NOPALIGN  HERE DUP $4E ADR, ; IMMEDIATE COMPILING
+: JOIN   ( from to -- )   OVER SWAP !BRANCH ;
 
 : LINK, ;
 : UNLINK,   $54 C, ;
