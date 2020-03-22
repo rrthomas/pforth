91106adf8e1fc35db7229f149ef295f0ba0adb16
Reuben Thomas <rrt@sc3d.org>
2020-03-22 13:22:08 +0000

    Mit: work with arbitrary 'FORTH on load
    Update PRE-INITIALIZE to use absolute addresses.
    
    Make PRIMITIVE-RP relocatable (it previously was hard-wired to low memory!);
    make PRIMITIVE-LINK, and PRIMITIVE-UNLINK, compile relocatable code.
    
    Use command-line argument addresses directly from Mit, rather than copying
    them into the memory.
    
    Update numbering of LIBMIT-PRIMITIVEs, which was not quite correct (but
    until now didn’t matter, because we weren’t using the incorrectly-numbered
    ones).

--- src/mit/fileio.fs
+++ src/mit/fileio.fs
@@ -63,25 +63,9 @@ PREVIOUS
 : REPOSITION-FILE   -ROT  D>OFF_T SEEK_SET LSEEK  OFF_T>D  -1 =  SWAP -1 =  AND ;
 : FILE-SIZE   FILE_SIZE  >R OFF_T>D R> ;
 : RESIZE-FILE   >R D>OFF_T R>  RESIZE_FILE ;
-: STRLEN   ( c-addr u1 -- u2 ) \ FIXME: Use C extra instruction
-   TUCK 0 ?DO
-      DUP I + C@ 0= IF
-         2DROP I  UNLOOP EXIT
-      THEN
-   LOOP  DROP ;
-: ARG-COPY   ( n adr u -- len )
-   DUP >R  ROT >R                       \ ( adr u )
-   OVER SWAP                            \ ( adr adr u )
-   MIT_CURRENT_STATE                    \ ( adr adr u ptr:mit_state* )
-   NATIVE_ADDRESS_OF_RANGE              \ ( adr dest:char* )
-   R> _ARG                              \ ( adr dest:char* src:char* )
-   R> STRNCPY                           \ ( adr dest:char* )
-   NATIVE-POINTER-CELLS 0 DO  DROP  LOOP
-   DUP STRLEN ;
-CREATE ARG-BUFFER  256 ALLOT \ FIXME: this is ugly
 : ABSOLUTE-ARG   ( u1 -- c-addr u2 )
    TOTAL-ARGS OVER > IF \ u1
-      ARG-BUFFER TUCK 256 ARG-COPY
+      _ARG  DUP STRLEN
    ELSE
       DROP  0 0
    THEN ;
