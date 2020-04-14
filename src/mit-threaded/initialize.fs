8c06c666faad3a872b6d6eb5748aba64a28ada02
Reuben Thomas <rrt@sc3d.org>
2020-04-14 15:03:33 +0100

    Mit: disable mit-threaded
    Currently this back-end is not relocatable. To work, RELOCATE itself must be
    able to run without needing relocation, for example, by being compiled as
    native Mit code. Leave a FIXME.

--- src/mit-threaded/initialize.fs
+++ src/mit-threaded/initialize.fs
@@ -6,6 +6,8 @@
 \ THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER’S
 \ RISK.
 
+\ FIXME: To make this work again, RELOCATE needs to be compiled to native
+\ code.
 : INITIALIZE
    RELOCATE                          \ perform relocations (must be done first)
    DUP TO R0                         \ set R0 (RP already set)
@@ -15,17 +17,18 @@
 CODE PRE-INITIALIZE
 \ Assume that we were called by a call instruction at 'FORTH, and
 \ use our return address to calculate the new value of 'FORTH.
-MLIT MNEGATE MADD MLIT_PC_REL
-2 CELLS ,
+MLIT MNEGATE MADD MLIT_0
+#TARGET-CALL-CELLS CELLS ,
+MDUP MLIT_PC_REL MLIT_2              \ ret 2 CALL-CELLS - 'FORTH !
 ' 'FORTH >BODY OFFSET,
-MLIT_2 MSTORE MLIT MLIT_0            \ ret 2 CELLS - 'FORTH !
+MSTORE MLIT MADD MLIT_0
 ' MEMORY-SIZE >BODY @ ,
-MDUP MLIT_PC_REL MLIT_2 MSTORE       \ memory-limit RP !
+MDUP MLIT_PC_REL MLIT_2 MSTORE       \ memory-limit RP !; FIXME: constant!
 ' RP >BODY OFFSET,
-MLIT_PC_REL MLIT_2 MLOAD MLIT_0      \ FIXME: constant × 2!
+MLIT_PC_REL MLIT_2 MLOAD MLIT_0      \ FIXME: constant!
 ' 'FORTH >BODY OFFSET,               \ 'FORTH @ DUP
 MDUP MLIT_PC_REL MLIT_PC_REL MCALL
-HERE 0 ,
-' INITIALIZE OFFSET,                 \ ( memory-limit 'FORTH 'FORTH )
+HERE 0 ,                             \ ( memory-limit 'FORTH 'FORTH )
+' INITIALIZE OFFSET,
 END-CODE
 ALIGN  HERE OVER -  SWAP !
