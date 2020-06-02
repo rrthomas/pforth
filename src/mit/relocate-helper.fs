536ad5200fca6b6837e2c438e3c9bd82eb11802b
Reuben Thomas <rrt@sc3d.org>
2020-06-02 01:08:21 +0100

    Remove relocation types
    We no longer need a special type for Beetle addresses.

--- src/mit/relocate-helper.fs
+++
@@ -1,1 +0,0 @@
-../relocate-helper.fs
--- src/relocate.fs
+++ src/relocate.fs
@@ -8,14 +8,13 @@
 
 \ FIXME: Compile this with the mit back-end for mit-threaded so it can
 \ actually be used for mit-threaded!
-INCLUDE" relocate-helper.fs"
 
 \ Relocate literal addresses relative to 'FORTH using the given relocation
 \ table.
-\ N.B. RELOCATE and its callees (in particular RELOCATE-HELPER) must not use
-\ addresses other than those passed to it as arguments (except for those
-\ being processed as part of relocations, of course). In particular, this is
-\ why current-base is passed as an argument rather than executing 'FORTH.
+\ N.B. RELOCATE and its callees must not use addresses other than those
+\ passed to it as arguments (except for those being processed as part of
+\ relocations, of course). In particular, this is why current-base is passed
+\ as an argument rather than executing 'FORTH.
 \ FIXME: Make relocation entries use relative addresses, and do not update
 \ the relocations (only the base address).
 : RELOCATE ( current-base new-base 'table -- )
@@ -26,14 +25,8 @@ INCLUDE" relocate-helper.fs"
    2R> CELLS SWAP CELL+ TUCK + SWAP ?DO \ ( addr-offset loc-offset )
       2DUP                              \ copy offsets
       I @                               \ ( …loc-offset relocation )
-      DUP CELL 1- AND                   \ ( …loc-offset relocation type )
-      SWAP CELL 1- INVERT AND           \ ( …loc-offset type address )
-      ROT +                             \ ( …addr-offset type address' )
-      SWAP ?DUP IF                      \ if type is non-zero, use helper
-         RELOCATE-HELPER
-      ELSE
-         +!                             \ otherwise simply relocate
-      THEN
+      SWAP +                            \ ( …addr-offset address' )
+      +!                                \ perform relocation
       OVER I +!                         \ update the relocation
    CELL +LOOP  2DROP                    \ drop offsets
    2R> ! ;                              \ update table base
