16a9b5a4b5d51b125e9b71c81ca4bb165e128ddf
Reuben Thomas <rrt@sc3d.org>
2020-06-02 01:02:22 +0100

    Use CURRENT-LITERAL, to define ADDRESS-LITERAL portably
    Inline its definition in make.fs.

--- src/add-relocate-helpers.fs
+++
@@ -1,9 +0,0 @@
-\ (c) Reuben Thomas 2019-2020
-\
-\ The package is distributed under the GNU GPL version 3, or, at your
-\ option, any later version.
-\
-\ THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER’S
-\ RISK.
-
-: ADDRESS-LITERAL   ALIGN HERE ADD-RELOCATION  POSTPONE LITERAL ; IMMEDIATE
