d9124d18d460ad59c6ca705c19837f3dcf2494bb
Reuben Thomas <rrt@sc3d.org>
2020-06-30 06:11:29 +0100

    extras/tests.fs: turn test error into non-zero exit status

--- extras/tests.fs
+++ extras/tests.fs
@@ -1,5 +1,9 @@
 \ Load and run John Hopkins test suite
 
+\ Turn exceptions into error exit
+: HALT-HANDLER   HALT ;
+' HALT-HANDLER 'THROW!
+
 \ Compatibility definition
 /LINE CONSTANT /L
 \ Load and run tests
