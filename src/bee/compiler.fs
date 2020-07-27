18dcfec12ac71f8aaf1e2d335a9f36675920027d
Reuben Thomas <rrt@sc3d.org>
2020-07-27 14:10:50 +0100

    bee/compiler.fs: factor !BRANCH out of JOIN and make it use OFFSET
    More similar to Beetle.

--- src/bee/compiler.fs
+++ src/bee/compiler.fs
@@ -12,17 +12,18 @@
 
 : NOPALIGN   ALIGN ;
 
+: OFFSET   ( from to -- offset )   >-< ;
+: CALL   ( at from to -- )   OFFSET SWAP ! ;
+
 : @BRANCH   ( from -- to )   DUP @ 2 ARSHIFT + $FFFFFFFC AND ;
+: !BRANCH   ( at from to -- )   OFFSET 2 LSHIFT  OVER @ $F AND  OR  SWAP ! ;
 : COMPILE,   DUP >INFO 2 + C@  ?DUP IF  0 DO  DUP @ ,  CELL+  LOOP  DROP
    ELSE HERE - ,  THEN ;
 
 : AHEAD   HERE  3 ,  ( FIXME: OP2_JUMPI 2 LSHIFT OP_LEVEL2 OR ) ; IMMEDIATE COMPILING
 : IF   HERE  7 ,   ( FIXME: OP2_JUMPZI 2 LSHIFT OP_LEVEL2 OR) ; IMMEDIATE COMPILING
-: JOIN   ( from to -- )   OVER - 2 LSHIFT  OVER @ $F AND  OR  SWAP ! ;
+: JOIN   ( from to -- )   OVER SWAP !BRANCH ;
 
 : LINK, ;
 : UNLINK,   $FF , ( FIXME: BRET ) ; COMPILING
 : LEAVE, ;
-
-: OFFSET   ( from to -- offset )   >-< ;
-: CALL   ( at from to -- )   OFFSET SWAP ! ;
