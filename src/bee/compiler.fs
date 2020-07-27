5f0acd740bbae1b764389a15baf24271e4905724
Reuben Thomas <rrt@sc3d.org>
2020-07-27 14:05:52 +0100

    Bee: move some control words from compiler-postpone.fs into compiler.fs
    These words (no longer) use POSTPONE, so don’t have to be in
    compiler-postpone.fs. This makes the code more similar between the Beetle
    and Bee backends.

--- src/bee/compiler.fs
+++ src/bee/compiler.fs
@@ -13,9 +13,16 @@
 : NOPALIGN   ALIGN ;
 
 : @BRANCH   ( from -- to )   DUP @ 2 ARSHIFT + $FFFFFFFC AND ;
-: JOIN   ( from to -- )   OVER - 2 LSHIFT  OVER @ $F AND  OR  SWAP ! ;
 : COMPILE,   DUP >INFO 2 + C@  ?DUP IF  0 DO  DUP @ ,  CELL+  LOOP  DROP
    ELSE HERE - ,  THEN ;
+
+: AHEAD   HERE  3 ,  ( FIXME: OP2_JUMPI 2 LSHIFT OP_LEVEL2 OR ) ; IMMEDIATE COMPILING
+: IF   HERE  7 ,   ( FIXME: OP2_JUMPZI 2 LSHIFT OP_LEVEL2 OR) ; IMMEDIATE COMPILING
+: JOIN   ( from to -- )   OVER - 2 LSHIFT  OVER @ $F AND  OR  SWAP ! ;
+
+: LINK, ;
+: UNLINK,   $FF , ( FIXME: BRET ) ; COMPILING
 : LEAVE, ;
+
 : OFFSET   ( from to -- offset )   >-< ;
 : CALL   ( at from to -- )   OFFSET SWAP ! ;
