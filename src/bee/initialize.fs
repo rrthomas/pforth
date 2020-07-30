e4cf2fe910dc8a18b3db8fcf478df32d0ead5565
Reuben Thomas <rrt@sc3d.org>
2020-07-30 09:55:29 +0100

    Make compiler output Bee assembly (disabling all other back-ends)

--- src/bee/initialize.fs
+++ src/bee/initialize.fs
@@ -11,6 +11,8 @@
    \ use our return address to calculate the new value of 'FORTH.
    R> CELL-  TO 'FORTH
    MEMORY@ M0@ +
-   [ HERE 0 ,  DUP ] \ value of HERE
+   [ HERE  .ASM( pushreli END_OF_IMAGE)  0 RAW,  DUP ] \ value of HERE
    START ;
-ALIGN  HERE >-<  OP_PUSHRELI OR  SWAP ! \ FIXME: add !OFFSET
+ALIGN
+.ASM( END_OF_IMAGE: )
+HERE >-<  OP_PUSHRELI OR  SWAP ! \ FIXME: add !OFFSET
